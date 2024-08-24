ecdf0826067f8edf5143f054f730fd451c5da632
Spardha-hash <spardha5102000@gmail.com>
2024-08-25 01:06:28 +0530

    SignUp Page Added

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,35 +1,63 @@
-console.log("script loaded");
+console.log("Script loaded");
 
+// change theme work
 let currentTheme = getTheme();
+//initial -->
+
 document.addEventListener("DOMContentLoaded", () => {
-    changeTheme();
+  changeTheme();
 });
 
-function changeTheme(){
-    changePageTheme(currentTheme, currentTheme);
-    const changeThemeButton = document.querySelector("#theme_change_button");
-    const oldTheme = currentTheme;
-    changeThemeButton.addEventListener("click", (event) => {
-        console.log("change theme button clicked");
-        if(currentTheme == "dark"){
-            currentTheme="light";
-        }
-        else{
-            currentTheme= "dark";
-        }
-        changePageTheme(currentTheme, oldTheme); 
-    });
+//TODO:
+function changeTheme() {
+  //set to web page
+
+  changePageTheme(currentTheme, "");
+  //set the listener to change theme button
+  const changeThemeButton = document.querySelector("#theme_change_button");
+
+  changeThemeButton.addEventListener("click", (event) => {
+    let oldTheme = currentTheme;
+    console.log("change theme button clicked");
+    if (currentTheme === "dark") {
+      //theme ko light
+      currentTheme = "light";
+    } else {
+      //theme ko dark
+      currentTheme = "dark";
+    }
+    console.log(currentTheme);
+    changePageTheme(currentTheme, oldTheme);
+  });
 }
-function setTheme(theme){
-    localStorage.setItem("theme", theme);
+
+//set theme to localstorage
+function setTheme(theme) {
+  localStorage.setItem("theme", theme);
 }
-function getTheme(){
-    let theme = localStorage.getItem("theme");
-    return theme ? theme : "light";
+
+//get theme from localstorage
+function getTheme() {
+  let theme = localStorage.getItem("theme");
+  return theme ? theme : "light";
 }
-function changePageTheme(theme, oldTheme){
-        setTheme(theme);
-        document.querySelector("html").classList.remove(oldTheme);
-        document.querySelector("html").classList.add(currentTheme);
-        document.querySelector("#theme_change_button").querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
+
+//change current page theme
+function changePageTheme(theme, oldTheme) {
+  //localstorage mein update karenge
+  setTheme(currentTheme);
+  //remove the current theme
+
+  if (oldTheme) {
+    document.querySelector("html").classList.remove(oldTheme);
+  }
+  //set the current theme
+  document.querySelector("html").classList.add(theme);
+
+  // change the text of button
+  document
+    .querySelector("#theme_change_button")
+    .querySelector("span").textContent = theme == "light" ? "Dark" : "Light";
 }
+
+//change page change theme
